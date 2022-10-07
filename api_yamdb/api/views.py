b807e5d4c9e9b4f98911ae1ebddbf7de28e92ab5
Илья Шустров <shustrov.i@ya.ru>
2022-10-07 05:26:16 +0300

    fixed all sh t with flake8

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -11,7 +11,7 @@ from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
-from reviews.models import Category, Genre, Title, Review, User
+from reviews.models import Category, Genre, Review, Title, User
 
 from .filters import TitleFilter
 from .permissions import (IsAdminOrReadOnly, IsAdminOrSuperuser,
